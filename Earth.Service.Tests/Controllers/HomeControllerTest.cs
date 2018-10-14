2eb3cdbd48daf8f2739f26cef7d0e63746c4d1ea
sanjeet2126 <sanjeet2126@SANJEET2126>
2018-10-14 23:08:25 +0530

    Add project files.

---
+++ Earth.Service.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Earth.Service;
+using Earth.Service.Controllers;
+
+namespace Earth.Service.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+           
+        }
+    }
+}
